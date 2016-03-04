fb89f83b53c195ff8da8c35da1b92c31e43a3250
Андрей Кумыков <aakumykov@yandex.ru>
2016-03-04 17:56:35 +0700

    валидация (с подсветкой) связанного объекта

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,9 @@
 class Person < ActiveRecord::Base
 	has_many :addresses
 	accepts_nested_attributes_for :addresses, allow_destroy: true, reject_if: lambda {|attributes| (attributes[:kind].blank? || attributes[:street].blank?) }
+
+	validates :name, presence: true
+
+	#validates :addresses, presence: true
+	validates_associated :addresses
 end
