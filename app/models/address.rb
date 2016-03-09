2d0c9a0d2b576fcad5173e00c909963d44301631
Андрей Кумыков <aakumykov@yandex.ru>
2016-03-09 16:56:40 +0700

    разбираюсь

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,9 +1,8 @@
 class Address < ActiveRecord::Base
 	belongs_to :person
-	accepts_nested_attributes_for :person, allow_destroy: true, reject_if: lambda {|attributes| attributes[:name].blank? }
-
+	accepts_nested_attributes_for :person, allow_destroy: false#, reject_if: lambda {|attributes| attributes[:name].blank? }
 	validates_associated :person#, if: "person_id.blank?"
-	validates :person_id, numericality: { only_integer: true }, if: "name.blank?"
-
+	
 	validates :street, presence: true
+	validates :person_id, numericality: { only_integer: true }#, if: "name.blank?"
 end
