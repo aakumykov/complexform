123e88d5531082cbc1d85c79f6472589371cd6c4
Андрей Кумыков <aakumykov@yandex.ru>
2016-03-09 15:11:29 +0700

    Не принимает атрибут :name

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,8 +1,9 @@
 class Address < ActiveRecord::Base
 	belongs_to :person
-	accepts_nested_attributes_for :person, allow_destroy: true
-	validates_associated :person
+	accepts_nested_attributes_for :person, allow_destroy: true, reject_if: lambda {|attributes| attributes[:name].blank? }
+
+	validates_associated :person#, if: "person_id.blank?"
+	validates :person_id, numericality: { only_integer: true }, if: "name.blank?"
 
 	validates :street, presence: true
-	#validates :person_id, presence: true
 end
