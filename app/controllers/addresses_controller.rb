43e2f3f3b56b97c51fba11435f57c3d86251a1ec
Андрей Кумыков <aakumykov@yandex.ru>
2016-03-10 10:15:02 +0700

    Понял условные валидации

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -26,11 +26,21 @@ class AddressesController < ApplicationController
   # POST /addresses
   # POST /addresses.json
   def create
-    @address = Address.new(address_params)
+	@address = Address.new(address_params)
+	
+	puts "===== @address.attributes ====="
+	@address.attributes.each { |k,v,|
+		puts "#{k} => #{v}"
+	}
+	
+	puts "===== @address.person.attributes =====>"
+	@address.person.attributes.each { |k,v,|
+		puts "#{k} => #{v}"
+	}
 
     respond_to do |format|
       if @address.save
-        format.html { redirect_to @address, notice: 'Address was successfully created.' }
+        format.html { redirect_to addresses_path, notice: 'Address was successfully created.' }
         format.json { render :show, status: :created, location: @address }
       else
         format.html { render :new }
@@ -44,7 +54,7 @@ class AddressesController < ApplicationController
   def update
     respond_to do |format|
       if @address.update(address_params)
-        format.html { redirect_to @address, notice: 'Address was successfully updated.' }
+        format.html { redirect_to addresses_path, notice: 'Address was successfully updated.' }
         format.json { render :show, status: :ok, location: @address }
       else
         format.html { render :edit }
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,8 +1,18 @@
 class Address < ActiveRecord::Base
+
 	belongs_to :person
-	accepts_nested_attributes_for :person, allow_destroy: false#, reject_if: lambda {|attributes| attributes[:name].blank? }
-	validates_associated :person#, if: "person_id.blank?"
 	
 	validates :street, presence: true
-	validates :person_id, numericality: { only_integer: true }#, if: "name.blank?"
+	#validates :person_id, numericality: { only_integer: true }#, if: "person.name.blank?"
+	
+	accepts_nested_attributes_for :person, allow_destroy: false#, reject_if: lambda {|attributes| attributes[:name].blank? }
+	validates_associated :person, unless: Proc.new { |a| 
+		puts "===== validates_associated, a =====> #{a}, #{a.person}" 
+		a.attributes.each do |k,v|
+			puts "#{k} => #{v}"
+		end
+		a.person.attributes.each do |k,v|
+			puts "#{k} => #{v}"
+		end
+	}
 end
